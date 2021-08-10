c1e63d54d4e66fe55f21a7104fb7d7b4c73ebcbf
shakamoushie <88389632+shakamoushie@users.noreply.github.com>
2021-08-10 11:59:24 +0530

    Add files via upload

---
+++ tstapp.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from streamlit import caching
+import pandas as pd
+import altair as alt
+from st_aggrid import AgGrid
+
+st.set_page_config(page_title = "SIRO Vista", page_icon="./Sirologo.ico", layout = "wide", initial_sidebar_state = "expanded")
+
+# dataframe settings
+pd.set_option("display.max_columns", None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------
+caching.clear_cache()
+df = (pd.read_excel(("./" + "vtmp.xlsx"), "wsdta"))
+
+sidlst = df["SCNID"].values.tolist()
+sidlst = list(set(sidlst))    # rmv dupes
+sidlst = [item for item in sidlst if not(pd.isnull(item)) == True]   # rmv NaN from a list
+sidlst.sort()
+sidlst.insert(0, "")     # insert blank entry to the top of the list
+
+tmpsid = st.selectbox("Choose a Subject ID: ", sidlst, index=0)
+if tmpsid != "":
+    AgGrid(df[(df["SCNID"] == tmpsid)])
+
+    cht1 = alt.Chart(df[(df["SCNID"] == tmpsid)])
+    cht1 = cht1.mark_line(color = "blue", point = True)
+    cht1 = cht1.encode(alt.X("VISIT", title = "Visit"), alt.Y("SCORE", title = "Visit Value"))
+    cht1 = cht1.properties(width = 800, height = 300, title = "Reading per Visit")
+    st.write(cht1)
